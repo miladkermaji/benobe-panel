ef94b4221f3c7a47b3458491b561e5d1cf2c6eab
miladkermanji8639 <miladkermaji09@gmail.com>
2025-03-07 08:42:49 +0330

    fix some space

--- resources/views/admin/panel/dashboard/index.blade.php
+++ resources/views/admin/panel/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.panel.layouts.master')
 @section('styles')
- <link type="text/css" href="{{ asset('dr-assets/panel/css/panel.css') }}" rel="stylesheet" />
+ <link type="text/css" href="{{ asset('admin-assets/panel/css/panel.css') }}" rel="stylesheet" />
 @endsection
 @section('site-header')
  {{ 'به نوبه | پنل مدیریت' }}
