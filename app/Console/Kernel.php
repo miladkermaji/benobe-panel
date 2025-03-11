b4656c10f8dfff4c69c269a599518cf7e2f5889b
miladkermanji8639 <miladkermaji09@gmail.com>
2025-03-12 01:36:10 +0330

    fix some problem

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,9 +7,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [
-        \App\Console\Commands\MakeCrudLivewire::class,
-    ];
     /**
      * Define the application's command schedule.
      */
