3ec999922ba1f62e5700594b326c09a9c1488160
miladkermanji8639 <miladkermaji09@gmail.com>
2025-03-12 13:14:26 +0330

    fix namespace

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\Api\Auth\AuthController;
 
 
 
-Route::prefix('admin/auth')->group(function () {
-    Route::post('/login-register', [AuthController::class, 'loginRegister'])->name('api.admin.auth.login-register');
-    Route::post('/login-confirm/{token}', [AuthController::class, 'loginConfirm'])->name('api.admin.auth.login-confirm');
-    Route::post('/resend-otp/{token}', [AuthController::class, 'resendOtp'])->name('api.admin.auth.resend-otp');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('api.admin.auth.logout');
+Route::prefix('/auth')->group(function () {
+    Route::post('/login-register', [AuthController::class, 'loginRegister'])->name('api.auth.login-register');
+    Route::post('/login-confirm/{token}', [AuthController::class, 'loginConfirm'])->name('api.auth.login-confirm');
+    Route::post('/resend-otp/{token}', [AuthController::class, 'resendOtp'])->name('api.auth.resend-otp');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('api.auth.logout');
 });
 
 
