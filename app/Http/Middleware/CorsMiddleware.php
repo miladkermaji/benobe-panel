144996e25d88f08b392ffaf35cd0fc823a9d344d
miladkermanji8639 <miladkermaji09@gmail.com>
2025-03-12 13:38:38 +0330

    fix cors middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -1,16 +1,32 @@
 <?php
+
 namespace App\Http\Middleware;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 use Closure;
-use Illuminate\Http\Request;
 
-class Cors
+class CorsMiddleware
 {
-    public function handle(Request $request, Closure $next)
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+
+    public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+        $response = $next($request);
+
+        // اگر پاسخ یک فایل است، بدون تغییر آن را برگردان
+        if ($response instanceof BinaryFileResponse) {
+            return $response;
+        }
+
+        return $response->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
+
 }
