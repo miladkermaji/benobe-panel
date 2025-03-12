ac01a382e43c78eebfc800bfa15f45a3a67e2ac0
miladkermanji8639 <miladkermaji09@gmail.com>
2025-03-12 13:10:32 +0330

    add new route api and new package to laravel success

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\Admin\Auth;
+namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\Otp;
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\Admin\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\AuthController;
 
 
 
