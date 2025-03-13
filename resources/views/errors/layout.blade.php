b352f19f40bb1b6f0645d58e61b918f44f4ca9e9
miladkermanji8639 <miladkermaji09@gmail.com>
2025-03-13 23:27:15 +0330

    fix styles error pages

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'خطا - به نوبه' }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+     @vite(['resources/js/app.js', 'resources/css/app.css'])
     <style>
         @import url('{{ asset('app-assets/fonts/vazir/font-face.css') }}');
 
