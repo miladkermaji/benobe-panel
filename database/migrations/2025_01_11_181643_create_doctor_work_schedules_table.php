7a560a496db874e114c65ae9333542787eb1deec
miladkermanji8639 <miladkermaji09@gmail.com>
2025-04-22 00:53:01 +0330

    fix doctor work schedule migration

--- database/migrations/2025_01_11_181643_create_doctor_work_schedules_table.php
+++ database/migrations/2025_01_11_181643_create_doctor_work_schedules_table.php
@@ -34,6 +34,7 @@ return new class extends Migration {
             // مثال: [{"start": "08:00", "end": "12:00"}, {"start": "14:00", "end": "18:00"}]
 
             $table->json('appointment_settings')->nullable()->comment(' زمانبندی باز شدن نوبت ها');
+            $table->json('emergency_times')->nullable()->comment(' زمان های اورژانسی');
             // تنظیمات اختصاصی نوبت‌دهی برای هر روز
             // مثال: {"max_appointments": 10, "appointment_duration": 15}
 
