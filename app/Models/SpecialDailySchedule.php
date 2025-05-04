f4d85ebf75193ac53a4b2fb465bf1edc550757e9
miladkermanji8639 <miladkermaji09@gmail.com>
2025-05-04 16:25:06 +0330

    added emergency time and appointment setting to speciaDailyTable

--- app/Models/SpecialDailySchedule.php
+++ app/Models/SpecialDailySchedule.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use App\Models\Doctor;
@@ -11,6 +12,7 @@ class SpecialDailySchedule extends Model
 
     protected $fillable = [
         'doctor_id',
+        'clinic_id',
         'date',
         'work_hours',
         'appointment_settings',
@@ -20,11 +22,17 @@ class SpecialDailySchedule extends Model
     // تعیین اینکه `work_hours` به عنوان JSON ذخیره شود
     protected $casts = [
         'work_hours' => 'array',
+        'appointment_settings' => 'array',
+        'emergency_times' => 'array',
     ];
 
     // ارتباط با مدل دکتر
     public function doctor()
     {
         return $this->belongsTo(Doctor::class);
+    }
+    public function clinic()
+    {
+        return $this->belongsTo(Clinic::class);
     }
 }
