6759e5ef1cb45c1b93802821089def19117f851d
miladkermanji8639 <miladkermaji09@gmail.com>
2025-05-11 00:46:32 +0330

    fix responsive docto cliics all pages

--- resources/views/dr/panel/doctors-clinic/layouts/partials/head-tags.blade.php
+++ resources/views/dr/panel/doctors-clinic/layouts/partials/head-tags.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="icon" type="image/x-icon" href="{{ asset('app-assets/logos/favicon.ico') }}">
-@vite(['resources/js/app.js', 'resources/css/app.css'])
+@vite(['resources/css/app.css', 'resources/css/timepicker.css', 'resources/js/app.js', 'resources/js/timepicker.js'])
 
 <link href="{{ asset('dr-assets/panel/css/bootstrap.min.css') }}" rel="stylesheet">
 <script src="{{ asset('dr-assets/panel/js/home/bootstrap/bootstrap.popper.min.js') }}"></script>
@@ -11,7 +11,7 @@
 
 {{-- persian calander --}}
 
-{{-- persian calander --}}
+{{-- persian calander --}}timepicker
 <link rel="stylesheet" href="{{ asset('dr-assets/panel/css/dr-panel.css') }}">
 
 <script src="{{ asset('dr-assets/panel/js/jquery-3.4.1.min.js') }}"></script>
--- resources/views/dr/panel/doctors-clinic/layouts/partials/scripts.blade.php
+++ resources/views/dr/panel/doctors-clinic/layouts/partials/scripts.blade.php
@@ -9,8 +9,6 @@
 
 <script src="{{ asset('app-assets/js/select2/select2.js') }}"></script>
 <script src="{{ asset('dr-assets/panel/js/chart/chart.js') }}"></script>
-<script src="{{ asset('app-assets/js/timepicker/timepicker.js') }}"></script>
-<script src="{{ asset('app-assets/js/all-custom.js') }}"></script>
 <script type="text/javascript" src="{{ asset('dr-assets/panel/jalali-datepicker/jalalidatepicker.min.js') }}"></script>
 
 <script type="text/javascript" src="{{ asset('dr-assets/panel/jalali-datepicker/date.js') }}"></script>
@@ -20,23 +18,4 @@
 <script src="{{ asset('dr-assets/panel/js/moment/jalali-moment.browser.js') }}"></script>
 <script src="{{ asset('dr-assets/panel/js/toastr/toastr.min.js') }}"></script>
 <script src="{{ asset('dr-assets/panel/js/global-loader.js') }}"></script>
-<script>
-  function initializeTimepicker() {
 
-    const DOMElement = $(".timepicker-ui");
-
-    const options = {
-      clockType: '24h',
-      theme: 'basic',
-      mobile: true,
-      enableScrollbar: true,
-      disableTimeRangeValidation: false,
-      autoClose: true
-    };
-    DOMElement.each(function() {
-
-      const newTimepicker = new window.tui.TimepickerUI(this, options);
-      newTimepicker.create();
-    });
-  }
-</script>
