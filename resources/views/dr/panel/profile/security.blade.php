a84b1b3e066863851dc1ba5112ea590d9ba05ec1
miladkermanji8639 <miladkermaji09@gmail.com>
2025-05-10 14:49:21 +0330

    fix security ui updated

--- resources/views/dr/panel/profile/security.blade.php
+++ resources/views/dr/panel/profile/security.blade.php
@@ -2,7 +2,6 @@
 
 @section('styles')
   <link type="text/css" href="{{ asset('dr-assets/panel/css/panel.css') }}" rel="stylesheet" />
-  <link type="text/css" href="{{ asset('dr-assets/panel/profile/edit-profile.css') }}" rel="stylesheet" />
   <link type="text/css" href="{{ asset('dr-assets/panel/css/profile/security.css') }}" rel="stylesheet" />
   <style>
     .myPanelOption {
@@ -48,6 +47,72 @@
 <script src="{{ asset('dr-assets/panel/js/dr-panel.js') }}"></script>
 
 <script>
+  $(document).ready(function() {
+    let dropdownOpen = false;
+    let selectedClinic = localStorage.getItem('selectedClinic');
+    let selectedClinicId = localStorage.getItem('selectedClinicId');
+    if (selectedClinic && selectedClinicId) {
+      $('.dropdown-label').text(selectedClinic);
+      $('.option-card').each(function() {
+        if ($(this).attr('data-id') === selectedClinicId) {
+          $('.option-card').removeClass('card-active');
+          $(this).addClass('card-active');
+        }
+      });
+    } else {
+      localStorage.setItem('selectedClinic', 'مشاوره آنلاین به نوبه');
+      localStorage.setItem('selectedClinicId', 'default');
+    }
+
+    function checkInactiveClinics() {
+      var hasInactiveClinics = $('.option-card[data-active="0"]').length > 0;
+      if (hasInactiveClinics) {
+        $('.dropdown-trigger').addClass('warning');
+      } else {
+        $('.dropdown-trigger').removeClass('warning');
+      }
+    }
+    checkInactiveClinics();
+
+    $('.dropdown-trigger').on('click', function(event) {
+      event.stopPropagation();
+      dropdownOpen = !dropdownOpen;
+      $(this).toggleClass('border border-primary');
+      $('.my-dropdown-menu').toggleClass('d-none');
+      setTimeout(() => {
+        dropdownOpen = $('.my-dropdown-menu').is(':visible');
+      }, 100);
+    });
+
+    $(document).on('click', function() {
+      if (dropdownOpen) {
+        $('.dropdown-trigger').removeClass('border border-primary');
+        $('.my-dropdown-menu').addClass('d-none');
+        dropdownOpen = false;
+      }
+    });
+
+    $('.my-dropdown-menu').on('click', function(event) {
+      event.stopPropagation();
+    });
+
+    $('.option-card').on('click', function() {
+      var selectedText = $(this).find('.fw-bold.d-block.fs-15').text().trim();
+      var selectedId = $(this).attr('data-id');
+      $('.option-card').removeClass('card-active');
+      $(this).addClass('card-active');
+      $('.dropdown-label').text(selectedText);
+
+      localStorage.setItem('selectedClinic', selectedText);
+      localStorage.setItem('selectedClinicId', selectedId);
+      checkInactiveClinics();
+      $('.dropdown-trigger').removeClass('border border-primary');
+      $('.my-dropdown-menu').addClass('d-none');
+      dropdownOpen = false;
+
+      window.location.href = window.location.pathname + "?selectedClinicId=" + selectedId;
+    });
+  });
   $(document).ready(function() {
     function loadLogs(page = 1) {
       $.ajax({
