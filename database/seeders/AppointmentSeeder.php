6ca756d18729ba25abfea7b0ac533dab83360331
miladkermanji8639 <miladkermaji09@gmail.com>
2025-05-21 01:28:10 +0330

    adde seeders to project with factories

--- database/seeders/AppointmentSeeder.php
+++ database/seeders/AppointmentSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+
+// database/seeders/AppointmentSeeder.php
+
 namespace Database\Seeders;
 
 use App\Models\Appointment;
@@ -11,7 +14,12 @@ class AppointmentSeeder extends Seeder
     public function run(): void
     {
         Log::info('Starting Appointment Seeding...');
-        Appointment::factory()->count(300)->create();
+        $batchSize = 50; // تولید 50 نوبت در هر دسته
+        for ($i = 0; $i < 6; $i++) { // 6 دسته 50 تایی = 300 نوبت
+            Appointment::factory()->count($batchSize)->create();
+            Log::info("Seeded batch " . ($i + 1) . " of $batchSize appointments.");
+            gc_collect_cycles(); // جمع‌آوری زباله برای آزادسازی حافظه
+        }
         Log::info('Appointment Seeding Completed.');
     }
 }
