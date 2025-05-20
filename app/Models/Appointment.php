7dcdd3e53f933dc1f9ca1a45ec54b5146a413c62
miladkermanji8639 <miladkermaji09@gmail.com>
2025-05-21 01:26:07 +0330

    adde seeders to project with factories

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -7,10 +7,11 @@ use App\Models\Specialty;
 use Morilog\Jalali\Jalalian;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Appointment extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes,HasFactory;
     protected $fillable = [
         'doctor_id',
         'patient_id',
