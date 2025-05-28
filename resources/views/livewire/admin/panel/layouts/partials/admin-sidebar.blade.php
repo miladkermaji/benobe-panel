94acdb593dc889c344a3d9ce69fc6b2cd7c31c7e
miladkermanji8639 <miladkermaji09@gmail.com>
2025-05-28 21:12:30 +0330

    add appoint ment fees and subscription to project

--- resources/views/livewire/admin/panel/layouts/partials/admin-sidebar.blade.php
+++ resources/views/livewire/admin/panel/layouts/partials/admin-sidebar.blade.php
@@ -94,6 +94,27 @@
             <li class="item-li"><a href="#">ارتقا حساب پزشکان</a></li>
           </ul>
         </li>
+         <!-- حق عضویت -->
+        <li class="item-li i-courses">
+          <a href="#" class="d-flex justify-content-between w-100 align-items-center">
+            حق عضویت
+            <div class="d-flex justify-content-end w-100 align-items-center">
+              <svg width="6" height="9" class="svg-caret-left" viewBox="0 0 7 11" fill="none"
+                xmlns="http://www.w3.org/2000/svg" style="transition: transform 0.3s; transform: rotate(180deg);">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M0.658146 0.39655C0.95104 0.103657 1.42591 0.103657 1.71881 0.39655L6.21881 4.89655C6.5117 5.18944 6.5117 5.66432 6.21881 5.95721L1.71881 10.4572C1.42591 10.7501 0.95104 10.7501 0.658146 10.4572C0.365253 10.1643 0.365253 9.68944 0.658146 9.39655L4.62782 5.42688L0.658146 1.45721C0.365253 1.16432 0.365253 0.689443 0.658146 0.39655Z"
+                  fill="currentColor"></path>
+              </svg>
+            </div>
+          </a>
+          <ul class="drop-toggle d-none">
+            <li class="item-li"><a href="{{ route('admin.panel.user-subscriptions.index') }}">اشتراک‌ها</a></li>
+            <li class="item-li"><a href="{{ route('admin.panel.user-membership-plans.index') }}">طرح‌های عضویت</a>
+            </li>
+            <li class="item-li"><a href="{{ route('admin.panel.user-appointment-fees.index') }}">هزینه‌های
+                نوبت‌دهی</a></li>
+          </ul>
+        </li>
         <!-- مدیریت منشی‌ها -->
         <li class="item-li i-users">
           <a href="#" class="d-flex justify-content-between w-100 align-items-center">
@@ -257,27 +278,7 @@
             <li class="item-li"><a href="{{ route('admin.panel.doctor-services.index') }}">خدمات پزشکان</a></li>
           </ul>
         </li>
-        <!-- حق عضویت -->
-        <li class="item-li i-courses">
-          <a href="#" class="d-flex justify-content-between w-100 align-items-center">
-            حق عضویت
-            <div class="d-flex justify-content-end w-100 align-items-center">
-              <svg width="6" height="9" class="svg-caret-left" viewBox="0 0 7 11" fill="none"
-                xmlns="http://www.w3.org/2000/svg" style="transition: transform 0.3s; transform: rotate(180deg);">
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                  d="M0.658146 0.39655C0.95104 0.103657 1.42591 0.103657 1.71881 0.39655L6.21881 4.89655C6.5117 5.18944 6.5117 5.66432 6.21881 5.95721L1.71881 10.4572C1.42591 10.7501 0.95104 10.7501 0.658146 10.4572C0.365253 10.1643 0.365253 9.68944 0.658146 9.39655L4.62782 5.42688L0.658146 1.45721C0.365253 1.16432 0.365253 0.689443 0.658146 0.39655Z"
-                  fill="currentColor"></path>
-              </svg>
-            </div>
-          </a>
-          <ul class="drop-toggle d-none">
-            <li class="item-li"><a href="{{ route('admin.panel.user-subscriptions.index') }}">اشتراک‌ها</a></li>
-            <li class="item-li"><a href="{{ route('admin.panel.user-membership-plans.index') }}">طرح‌های عضویت</a>
-            </li>
-            <li class="item-li"><a href="{{ route('admin.panel.user-appointment-fees.index') }}">هزینه‌های
-                نوبت‌دهی</a></li>
-          </ul>
-        </li>
+       
         <!-- مدیریت مالی -->
         <li class="item-li i-courses">
           <a href="#" class="d-flex justify-content-between w-100 align-items-center">
