0d75e5530aa91a8a2b8b0a8e3c62ea1b40e972a1
miladkermanji8639 <miladkermaji09@gmail.com>
2025-05-29 00:42:30 +0330

    fix plan ui

--- resources/views/livewire/admin/panel/user-membership-plans/membership-plan-list.blade.php
+++ resources/views/livewire/admin/panel/user-membership-plans/membership-plan-list.blade.php
@@ -13,7 +13,7 @@
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
             <path d="M12 5v14M5 12h14" />
           </svg>
-          افزودن طرح جدید
+          افزودن  
         </a>
       </div>
     </div>
