a548035479d93a28ce45e00eaf8011806ef46a33
miladkermanji8639 <miladkermaji09@gmail.com>
2025-06-30 20:16:29 +0330

    add user subscripion some change and add logs to fix this problom stre data

--- app/Http/Controllers/Api/UserSubscriptionController.php
+++ app/Http/Controllers/Api/UserSubscriptionController.php
@@ -92,9 +92,8 @@ class UserSubscriptionController extends Controller
         $successRedirect = $frontendUrl . '/payment/success';
         $errorRedirect = $frontendUrl . '/payment/error';
 
-        $callbackUrl = route('api.v2.subscriptions.payment.callback', [], true);
         try {
-            $paymentResponse = $this->paymentService->pay($amount, $callbackUrl, $meta, $successRedirect, $errorRedirect);
+            $paymentResponse = $this->paymentService->pay($amount, null, $meta, $successRedirect, $errorRedirect);
 
             if ($paymentResponse instanceof \Shetabit\Multipay\RedirectionForm) {
                 return response()->json([
