886778de474b3bcdfbea9b561ccb4dd01fdce14a
miladkermanji8639 <miladkermaji09@gmail.com>
2025-06-30 22:02:11 +0330

    add user subscripion some change and add logs to fix this problom stre data

--- app/Http/Controllers/Api/UserSubscriptionController.php
+++ app/Http/Controllers/Api/UserSubscriptionController.php
@@ -92,11 +92,8 @@ class UserSubscriptionController extends Controller
         $successRedirect = $frontendUrl . '/payment/success';
         $errorRedirect = $frontendUrl . '/payment/error';
 
-        // تعیین آدرس صحیح کال‌بک برای سابسکریپشن
-        $callbackUrl = config('app.url', 'https://emr-benobe.ir') . '/api/v2/subscriptions/payment/callback';
-
         try {
-            $paymentResponse = $this->paymentService->pay($amount, $callbackUrl, $meta, $successRedirect, $errorRedirect);
+            $paymentResponse = $this->paymentService->pay($amount, null, $meta, $successRedirect, $errorRedirect);
 
             if ($paymentResponse instanceof \Shetabit\Multipay\RedirectionForm) {
                 return response()->json([
