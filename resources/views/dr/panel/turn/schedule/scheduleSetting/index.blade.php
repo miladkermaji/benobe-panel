7803109835c5eef8eeb9d23289b9b3667f947793
miladkermanji8639 <miladkermaji09@gmail.com>
2025-07-14 20:22:44 +0330

    fix some eror and improve myspecial days dr-panel

--- resources/views/dr/panel/turn/schedule/scheduleSetting/index.blade.php
+++ resources/views/dr/panel/turn/schedule/scheduleSetting/index.blade.php
@@ -44,22 +44,6 @@
         </div>
       </li>
       <li class="d-flex align-items-center cursor-pointer" tabindex="0" role="button"
-        onclick="location.href='{{ route('dr-manual_nobat_setting') }}'">
-        <div class="my-svg-setting-nobat">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-              stroke="#000000" stroke-width="1.5" />
-            <path d="M12 6V12L16 14" stroke="#000000" stroke-width="1.5" stroke-linecap="round"
-              stroke-linejoin="round" />
-          </svg>
-        </div>
-        <div class="ml-2">
-          <span class="fw-bold">تنظیمات نوبت دستی</span>
-          <p>مدیریت نوبت‌های دستی</p>
-        </div>
-      </li>
-      <li class="d-flex align-items-center cursor-pointer" tabindex="0" role="button"
         onclick="location.href='{{ route('doctor-blocking-users.index') }}'">
         <div class="my-svg-setting-nobat">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
