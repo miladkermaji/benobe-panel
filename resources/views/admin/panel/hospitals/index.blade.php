7601b008a3dac9b54901e319d9d3fdc7202f056c
miladkermanji8639 <miladkermaji09@gmail.com>
2025-07-20 15:14:25 +0330

    redesign admin-panel

--- resources/views/admin/panel/hospitals/index.blade.php
+++ resources/views/admin/panel/hospitals/index.blade.php
@@ -1,9 +1,4 @@
 @extends('admin.panel.layouts.master')
-
-@section('styles')
-  <link type="text/css" href="{{ asset('admin-assets/css/panel/hospital/hospital.css') }}" rel="stylesheet" />
-@endsection
-
 @section('site-header')
   {{ 'به نوبه | پنل مدیریت' }}
 @endsection
