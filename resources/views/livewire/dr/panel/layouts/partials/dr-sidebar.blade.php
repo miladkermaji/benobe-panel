ce573ea01fde1313b15bb07eea593001ecd19dad
miladkermanji8639 <miladkermaji09@gmail.com>
2025-07-23 00:51:21 +0330

    fix header  responsive

--- resources/views/livewire/dr/panel/layouts/partials/dr-sidebar.blade.php
+++ resources/views/livewire/dr/panel/layouts/partials/dr-sidebar.blade.php
@@ -1014,8 +1014,7 @@
       align-items: stretch;
       overflow-y: auto;
       transition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-      border-bottom-left-radius: 24px;
-      border-bottom-right-radius: 24px;
+
       box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
       backdrop-filter: blur(10px);
     }
