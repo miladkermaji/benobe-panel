60169b88461f329651b812d09ef9d27fb32bdc9d
miladkermanji8639 <miladkermaji09@gmail.com>
2025-08-03 18:55:22 +0330

    improved migrations

--- database/migrations/2025_03_09_070416_create_login_sessions_table.php
+++ database/migrations/2025_03_09_070416_create_login_sessions_table.php
@@ -4,24 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class () extends Migration {
     public function up(): void
     {
         Schema::create('login_sessions', function (Blueprint $table) {
             $table->id();
+            $table->string('sessionable_type')->nullable();
+            $table->unsignedBigInteger('sessionable_id')->nullable();
             $table->string('token', 60)->unique()->index();
-            $table->unsignedBigInteger('manager_id')->nullable();
-            $table->unsignedBigInteger('doctor_id')->nullable();
-            $table->unsignedBigInteger('secretary_id')->nullable();
-            $table->unsignedBigInteger('user_id')->nullable();
             $table->unsignedTinyInteger('step')->default(1); // 1: ورود موبایل، 2: OTP، 3: رمز عبور، 4: دو عاملی
             $table->timestamp('expires_at');
             $table->timestamps();
 
-            $table->foreign('manager_id')->references('id')->on('managers')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('doctor_id')->references('id')->on('doctors')->onDelete('cascade');
-            $table->foreign(columns: 'secretary_id')->references('id')->on('secretaries')->onDelete('cascade');
+            // اضافه کردن ایندکس برای پولی مورفیک
+            $table->index(['sessionable_type', 'sessionable_id']);
         });
     }
 
--- database/migrations/create_otps_table.php
+++ database/migrations/create_otps_table.php
@@ -12,17 +12,18 @@ return new class () extends Migration {
     {
         Schema::create('otps', function (Blueprint $table) {
             $table->id();
+            $table->string('otpable_type')->nullable();
+            $table->unsignedBigInteger('otpable_id')->nullable();
             $table->string('token');
-            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
-            $table->foreignId('doctor_id')->nullable()->constrained('doctors')->onDelete('cascade');
-            $table->foreignId('secretary_id')->nullable()->constrained('secretaries')->onDelete('cascade');
-            $table->foreignId('manager_id')->nullable()->constrained('managers')->onDelete('cascade');
             $table->string('otp_code');
             $table->string('login_id')->comment('email address or mobile number');
             $table->tinyInteger('type')->default(0)->comment('0 => mobile number , 1 => email');
             $table->tinyInteger('used')->default(0)->comment('0 => not used , 1 => used');
             $table->tinyInteger('status')->default(0);
             $table->timestamps();
+
+            // اضافه کردن ایندکس برای پولی مورفیک
+            $table->index(['otpable_type', 'otpable_id']);
 
             // اضافه کردن ایندکس‌ها
             $table->index('type');
