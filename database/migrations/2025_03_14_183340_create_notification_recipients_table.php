c6f5ebdc2a7aae68585ba6f5861c024552c9c694
miladkermanji8639 <miladkermaji09@gmail.com>
2025-08-05 21:50:03 +0330

    fix some uis responsive header and card mobile and tablet

--- database/migrations/2025_03_14_183340_create_notification_recipients_table.php
+++ database/migrations/2025_03_14_183340_create_notification_recipients_table.php
@@ -13,7 +13,8 @@ return new class () extends Migration {
         Schema::create('notification_recipients', function (Blueprint $table) {
             $table->id();
             $table->foreignId('notification_id')->constrained('notifications')->onDelete('cascade');
-            $table->morphs('recipient', null, null, 'recipient_id'); // برای انعطاف‌پذیری با جدول‌های مختلف (users, doctors, secretaries)
+            $table->string('recipient_type')->nullable(); // برای انعطاف‌پذیری با جدول‌های مختلف (users, doctors, secretaries)
+            $table->unsignedBigInteger('recipient_id')->nullable(); // Made nullable for single phone notifications
             $table->string('phone_number')->nullable(); // شماره تلفن برای ارسال تکی
             $table->boolean('is_read')->default(false); // آیا دیده شده
             $table->timestamp('read_at')->nullable(); // زمان دیده شدن
