07599eced36bf0966254da00ddec09d02c1f375d
miladkermanji8639 <miladkermaji09@gmail.com>
2025-08-07 11:29:06 +0330

    some change api login jwt improve and v2 auth to api

--- resources/views/livewire/admin/panel/layouts/partials/admin-sidebar.blade.php
+++ resources/views/livewire/admin/panel/layouts/partials/admin-sidebar.blade.php
@@ -11,7 +11,7 @@
         <div class="v-dialog__container" style="display: block;"></div>
         <div class="box__camera default__avatar"></div>
       </div>
-      <span class="profile__name sidebar-full-name d-block">
+      <span class="profile__name sidebar-full-name br">
         {{ $user ? $user->first_name . ' ' . $user->last_name : 'کاربر ناشناس' }}
       </span>
       <span class="fs-11 fw-bold" id="takhasos-txt">{{ $userType }}</span>
