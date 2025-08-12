b4a6c56d75377ea3876a033d7cad8db6504dec0f
miladkermanji8639 <miladkermaji09@gmail.com>
2025-08-13 00:21:21 +0330

    fix some login password

--- resources/views/mc/panel/layouts/partials/head-tags.blade.php
+++ resources/views/mc/panel/layouts/partials/head-tags.blade.php
@@ -17,7 +17,7 @@
 
 <script src="{{ asset('dr-assets/panel/js/jquery-3.4.1.min.js') }}"></script>
 <script src="{{ asset('dr-assets/panel/js/jquery-easing/1.4.1/jquery.easing.min.js') }}"></script>
-<link rel="stylesheet" href="{{ asset('dr-assets/panel/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('mc-assets/panel/css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('dr-assets/panel/css/responsive_991.css') }}" media="(max-width:991px)">
 <link rel="stylesheet" href="{{ asset('dr-assets/panel/css/responsive_768.css') }}" media="(max-width:768px)">
 <link rel="stylesheet" href="{{ asset('dr-assets/panel/css/font.css') }}">
